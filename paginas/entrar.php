03528897a9691069f35a28aaf19aeaa1648a8b9c
Edivaldo F.M. Júnior <50684038+edivaldo66@users.noreply.github.com>
2019-06-27 12:37:44 -0300

    Update entrar.php

--- paginas/entrar.php
+++ paginas/entrar.php
@@ -1,45 +1,52 @@
 <?php
+//session_start();
 
 if (isset($_POST["email"]) && isset($_POST["senha"])) {
 	$email = $_POST["email"];
 	$senha = $_POST["senha"];
 	$sql = "SELECT * FROM pandas WHERE email = '$email' AND senha = '$senha'";
 	$query = mysqli_query($link, $sql);
-	if($row = mysqli_fetch_assoc($query)){
-		
-		$_SESSION["id_usuario"] = $row["id"];
-		$_SESSION["nome_usuario"] = $row["nome"];
 
-		header("Location: ?pg=painel");
+if($row = mysqli_fetch_assoc($query)){
 
-	}
-	else{
-		echo "Usuário ou senha inválido!";
-	}
-	
+	$_SESSION["id_usuario"] = $row["id"];
+	$_SESSION["nome_usuario"] = $row["nome"];
+
+} else {
+
+echo '<div class="alert alert-danger" role="alert">Falhou!</div>';
+
 }
+}
+if(isset($_SESSION["id_usuario"])) {
+header("Location:index.php");
+}
+$email = NULL;
+$senha = NULL;
 
 ?>
 
-<h2 class="text-center">Entrar</h2>
 
-<form action="?pg=valida" method="POST">
 
 	<div class="row">
 		<div class="col-md-3">
 		</div>
 		
 		<div class="col-md-6">
+
+			<div class="jumbotron">
+
+				<h2 class="text-center" style="color: white;">Entrar</h2>
 			
 				<form action="?pg=entrar" method="POST">
 				  <div class="form-group">
 				    <label for="emailInput">Email</label>
-				    <input type="email" class="form-control" id="emailInput" name="emailInput" aria-describedby="emailHelp" placeholder="Digite seu email" value="<? $email ?>">
+				    <input type="email" class="form-control" id="emailInput" name="email" required="required" placeholder="Digite seu email" value="<?= $email ?>">
 				  </div>
 
 				  <div class="form-group">
 				    <label for="senhaInput">Senha</label>
-				    <input type="password" class="form-control" id="senhaInput" name="senhaInput" placeholder="Digite sua senha" value="<?= $senha ?>">
+				    <input type="password" class="form-control" id="senhaInput" name="senha" required="required" placeholder="Digite sua senha" value="<?= $senha ?>">
 				  </div>
 				  <div class="form-group form-check">
 				    <input type="checkbox" class="form-check-input" id="exampleCheck1">
@@ -51,6 +58,7 @@ if (isset($_POST["email"]) && isset($_POST["senha"])) {
 
 
 			<br>
+		</div>
 
 		</div>
 	<div class="col-md-6">
