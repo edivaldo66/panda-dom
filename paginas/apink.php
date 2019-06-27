6040fe2a7f77fb405ff3716fde7c230a4e2029d6
Edivaldo F.M. Júnior <50684038+edivaldo66@users.noreply.github.com>
2019-06-27 15:28:50 -0300

    Update apink.php

--- paginas/apink.php
+++ paginas/apink.php
@@ -1,1 +1,26 @@
-<h2>Sobre Apink</h2>
+<div class="jumbotron jumbotron-fluid">
+  <div class="container">
+  	<h3 style="color: white;">Sobre Apink</h3>
+
+	<hr style="color: white;" class="my-4">
+	
+    <p class="lead" style="color: white;"></p>
+
+    	<div class="jumbotron" style="background-color: #fff;">
+    		<div class="jumbotron" style="height: 350px;">
+				<img src="imagens/apink6.jpg" height="284" width="400" align="right">
+				<div class="col-md-7">
+					<h6 style="color: white;">Apink é um grupo feminino de kpop que estreou em 19 de abril de 2011 pela Plan A Entertainment (antiga A Cube Entertainment) composta pelas integrantes Park Chorong, Yoon Bomi, Jeong Eunji, Son Naeun, Kimm Namjoo e Oh Hayoung.</h6>
+				</div>
+
+    		</div>
+
+    		<div class="jumbotron">
+    						
+    		</div>
+
+    	</div>
+
+  </div>
+
+</div>
